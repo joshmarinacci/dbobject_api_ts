a6b8e277312a189f1c052c959a363aa397cfd8be
Josh Marinacci <joshua@marinacci>
2023-01-16 13:11:02 -0800

    remove path ref

--- src/mime.ts
+++ src/mime.ts
@@ -1,5 +1,3 @@
-import path from "path";
-
 const EXT_TO_MIME = {
     ".json":'application/json',
     ".pdf":"image/pdf"
@@ -7,8 +5,11 @@ const EXT_TO_MIME = {
 export function detect_mime(buff: Buffer, opaque: any, mime: any) {
     if (mime) return mime
     if (typeof opaque === 'string') {
-        let ext = path.extname(opaque)
-        if(EXT_TO_MIME[ext]) return EXT_TO_MIME[ext]
+        let n = opaque.lastIndexOf('.')
+        if(n > 0) {
+            let ext = opaque.substring(n)
+            if (EXT_TO_MIME[ext]) return EXT_TO_MIME[ext]
+        }
     }
     return "application/unknown"
 }
